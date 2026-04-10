cbd66b7af23ffc899c5cdd16eddfcedab58df74b
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-10 13:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn")
+let p = document.getElementById("counter")
+
+btn.addEventListener("mouseover",()=>{
+	btn.style.backgroundColor = "cyan"
+})
+
+btn.addEventListener("mouseout",()=>{
+	btn.style.backgroundColor = "#B2FFFF"
+})
+btn.addEventListener("click", ()=>{
+	let count = parseInt(p.innerText);
+	count += 1;
+	p.innerText = count;
+})
